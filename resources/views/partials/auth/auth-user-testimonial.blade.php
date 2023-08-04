ec3d5a09a6b04791cd042ff6616fdd0df0a5fc9f
root <root@campaign.mewar.ac.ae>
2023-08-04 18:12:23 +0530

    theme changes

--- resources/views/partials/auth/auth-user-testimonial.blade.php
+++ resources/views/partials/auth/auth-user-testimonial.blade.php
@@ -3,7 +3,6 @@
     <h3 class="mb-3 text-white">NBTE APPROVED HND TO DEGREE TOPUP PORTAL</h3>
     <p class="lead fw-normal"><i class="mdi mdi-format-quote-open"></i> This Dedicated Gateway for your NBTE approved Higher National Diploma to Bachelors Degree topup is an exclusive platform launched to provide you a world of opportunities <i class="mdi mdi-format-quote-close"></i>
     </p>
-    <h5 class="text-white">
-        - Director Admissions National Board for Technical Education
+    <h5 class="text-white">National Board for Technical Education
     </h5>
 </div>
