5d555a4d39aec777aea7bec080b08ece4f1c0407
Sam <sam@yopmail.com>
2023-09-02 14:57:01 +0530

    samer

--- resources/views/payments/manager/index.blade.php
+++ resources/views/payments/manager/index.blade.php
@@ -48,6 +48,7 @@
                                 <th>Date</th>
                                 <th>Student</th>
                                 <th>Application ID</th>
+                                <th>Email</th>
                                 <th>Reference</th>
                                 <th>Amount</th>
                                 <th>Action</th>
@@ -106,6 +107,7 @@
                     {data: 'created_at',        name: 'created_at'},
                     {data: 'student.name',       name: 'student.name'},
                     {data: 'application_id',    name: 'application_id'},
+                                        {data: 'student.email',              name: 'student.email'},
                     {data: 'reference',         name: 'reference'},
                     {data: 'amount',            name: 'amount'},
                     {data: 'download',            name: 'download'}
